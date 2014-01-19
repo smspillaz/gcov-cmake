837352e73d6592310438016f11fb07e8ac2fd51f
Sam Spilsbury <smspillaz@gmail.com>
2014-01-19 17:52:57 -0200

    Fix some style issues and add reporting

--- GCovUtilities.cmake
+++ GCovUtilities.cmake
@@ -10,7 +10,7 @@ find_program (GCOV_EXECUTABLE gcov)
 
 if (GCOV_EXECUTABLE)
 
-	mark_as_advanced (GCOV_EXECUTABLE)
+    mark_as_advanced (GCOV_EXECUTABLE)
 
 else (GCOV_EXECUTABLE)
 
@@ -21,8 +21,8 @@ endif (GCOV_EXECUTABLE)
 
 if (NOT CMAKE_COMPILER_IS_GNUCXX OR NOT CMAKE_COMPILER_IS_GNUCC)
 
-	message (STATUS "Compiler is not gcc or g++, coverage reports will not be "
-	                "enabled")
+    message (STATUS "Compiler is not gcc or g++, coverage reports will not be "
+                    "enabled")
 
 endif (NOT CMAKE_COMPILER_IS_GNUCXX OR NOT CMAKE_COMPILER_IS_GNUCC)
 
@@ -33,27 +33,31 @@ set (FORCE_DEBUG_SYMBOLS_AND_NOOPT OFF)
 
 if (ENABLE_COVERAGE OR ENABLE_PROFILING)
 
-	set (FORCE_DEBUG_SYMBOLS_AND_NOOPT ON)
+    set (FORCE_DEBUG_SYMBOLS_AND_NOOPT ON)
 
 endif (ENABLE_COVERAGE OR ENABLE_PROFILING)
 
 if (FORCE_DEBUG_SYMBOLS_AND_NOOPT)
 
-	set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O0")
-	set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -O0")
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O0")
+    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -O0")
 
 endif (FORCE_DEBUG_SYMBOLS_AND_NOOPT)
 
 if (ENABLE_COVERAGE)
 
-	set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftest-coverage")
-	set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ftest-coverage")
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftest-coverage")
+    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ftest-coverage")
+
+    message (STATUS "Code coverage reporting enabled")
 
 endif (ENABLE_COVERAGE)
 
 if (ENABLE_PROFILING)
 
-	set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs")
-	set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fprofile-arcs")
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs")
+    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fprofile-arcs")
+
+    message (STATUS "Code profiling counters enabled")
 
 endif (ENABLE_PROFILING)
